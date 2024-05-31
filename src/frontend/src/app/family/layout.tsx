9e30017ffc4fd2a201ba3a56fb0341830583d5ee
astrhsptl <dctrback@bk.ru>
2024-05-31 15:28:52 +0300

    test

--- src/frontend/src/app/family/layout.tsx
+++ src/frontend/src/app/family/layout.tsx
@@ -1,14 +1,13 @@
-import '@/shared/styles/base.css';
-import { Metadata } from 'next';
+import { NavigationPanel } from '@/widgets/navigation-panel';
 
-interface RootLayoutProps {
+interface LayoutProps {
   children: React.ReactNode;
 }
-
-export const metadata: Metadata = {
-  manifest: '/manifest.json',
-};
-
-export default async function Layout({ children }: RootLayoutProps) {
-  return <main>{children}</main>;
+export default async function Layout({ children }: LayoutProps) {
+  return (
+    <>
+      {children}
+      <NavigationPanel />
+    </>
+  );
 }
--- src/frontend/src/app/family/template.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-interface BaseTemplateProps {
-  children: ReactNode;
-}
-
-export default function Template({ children }: BaseTemplateProps) {
-  return <>{children}</>;
-}
